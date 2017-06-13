5c328ddf75d54089552c434972ae5fd5bea778a5
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2017-06-13 18:20:33 +0900

    Declare vectors that contain all enum elements
      ref #12

--- lib/dsl/ast.cc
+++ lib/dsl/ast.cc
@@ -2,6 +2,34 @@
 
 using namespace dsl;
 
+std::vector<Function> dsl::all_functions = {Function::Head, Function::Last, Function::Take, Function::Drop,
+                                            Function::Access, Function::Minimum, Function::Maximum, Function::Reverse,
+                                            Function::Sort, Function::Sum, Function::Map, Function::Filter,
+                                            Function::Count, Function::ZipWith, Function::Scanl1, Function::ReadInt,
+                                            Function::ReadList};
+
+std::vector<PredicateLambda> dsl::all_predicate_lambdas = {PredicateLambda::IsPositive,
+                                                           PredicateLambda::IsNegative,
+                                                           PredicateLambda::IsEven,
+                                                           PredicateLambda::IsOdd};
+
+std::vector<TwoArgumentsLambda> dsl::all_two_arguments_lambdas = {TwoArgumentsLambda::Plus,
+                                                                  TwoArgumentsLambda::Minus,
+                                                                  TwoArgumentsLambda::Multiply,
+                                                                  TwoArgumentsLambda::Min,
+                                                                  TwoArgumentsLambda::Max};
+
+std::vector<OneArgumentLambda> dsl::all_one_argument_lambdas = {OneArgumentLambda::Plus1,
+                                                                OneArgumentLambda::Minus1,
+                                                                OneArgumentLambda::MultiplyMinus1,
+                                                                OneArgumentLambda::Multiply2,
+                                                                OneArgumentLambda::Multiply3,
+                                                                OneArgumentLambda::Multiply4,
+                                                                OneArgumentLambda::Divide2,
+                                                                OneArgumentLambda::Divide3,
+                                                                OneArgumentLambda::Divide4,
+                                                                OneArgumentLambda::Pow2};
+
 Argument::Argument(Variable variable) : m_argument(variable) {}
 Argument::Argument(TwoArgumentsLambda lambda) : m_argument(static_cast<uint32_t>(lambda)) {}
 Argument::Argument(PredicateLambda lambda) : m_argument(static_cast<uint32_t>(lambda)) {}
