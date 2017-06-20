002c085e80a10be72ad9d501951c0211a84453e0
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2017-06-20 18:21:17 +0900

    Implement sort and add enumeration
      ref #7

--- include/find-program.h
+++ include/find-program.h
@@ -7,3 +7,4 @@
 #include "example-generator.h"
 
 std::experimental::optional<dsl::Program> dfs(size_t max_length, const Attribute &attr, const std::vector<Example> &examples);
+std::experimental::optional<dsl::Program> sort_and_add(size_t max_length, const Attribute &attr, const std::vector<Example> &examples);
