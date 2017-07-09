d7792aefed42ad032b6109e5970834a21dc97c70
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2017-07-09 13:21:23 +0900

    Fix dataset-generator's test
      ref #20

--- test/dataset-generator-test.cc
+++ test/dataset-generator-test.cc
@@ -44,30 +44,43 @@ TEST(DatasetTest, InsertTest) {
 
     vector<Example> examples1 = {
             {{Value({0, 1})}, Value(0)},
+            {{Value({-1, 10})}, Value(-1)},
+            {{Value({-1, 10})}, Value(-1)},
+            {{Value({-1, 10})}, Value(-1)},
             {{Value({-1, 10})}, Value(-1)}
     };
 
     vector<Example> examples2 = {
             {{Value({0, 1})}, Value(0)},
+            {{Value({-1, 10})}, Value(-1)},
+            {{Value({-1, 10})}, Value(-1)},
+            {{Value({-1, 10})}, Value(-1)},
             {{Value({-1, 10})}, Value(-1)}
     };
 
     vector<Example> examples3 = {
             {{Value({0, 1})}, Value(1)},
+            {{Value({-1, 10})}, Value(10)},
+            {{Value({-1, 10})}, Value(10)},
+            {{Value({-1, 10})}, Value(10)},
             {{Value({-1, 10})}, Value(10)}
     };
 
     vector<Example> examples4 = {
+            {{Value(2), Value({0, 1, 2})}, Value({0, 1})},
+            {{Value(2), Value({0, 1, 2})}, Value({0, 1})},
+            {{Value(2), Value({0, 1, 2})}, Value({0, 1})},
+            {{Value(2), Value({0, 1, 2})}, Value({0, 1})},
             {{Value(2), Value({0, 1, 2})}, Value({0, 1})}
     };
 
     dataset.insert(p2, examples2);
     EXPECT_EQ(1, dataset.programs.size());
-    EXPECT_EQ(2, dataset.size);
+    EXPECT_EQ(1, dataset.size);
 
     dataset.insert(p1, examples1);
     EXPECT_EQ(1, dataset.programs.size());
-    EXPECT_EQ(2, dataset.size);
+    EXPECT_EQ(1, dataset.size);
     for (auto &x: dataset.programs) {
         for (auto &y: x.second) {
             EXPECT_EQ(2, y.first.size());
@@ -76,18 +89,18 @@ TEST(DatasetTest, InsertTest) {
 
     dataset.insert(p3, examples3);
     EXPECT_EQ(1, dataset.programs.size());
-    EXPECT_EQ(4, dataset.size);
+    EXPECT_EQ(2, dataset.size);
 
     dataset.insert(p4, examples4);
     EXPECT_EQ(2, dataset.programs.size());
-    EXPECT_EQ(5, dataset.size);
+    EXPECT_EQ(3, dataset.size);
 
 
     Dataset dataset2;
     dataset2.insert(p1, examples1);
     dataset2.insert(p2, examples2);
     EXPECT_EQ(1, dataset2.programs.size());
-    EXPECT_EQ(2, dataset2.size);
+    EXPECT_EQ(1, dataset2.size);
     for (auto &x: dataset2.programs) {
         for (auto &y: x.second) {
             EXPECT_EQ(2, y.first.size());
