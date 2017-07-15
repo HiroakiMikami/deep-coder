faa3f2ccdd8b49435fd791fe5fe9ffe809ef3ed4
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2017-07-16 07:50:01 +0900

    Use environment variable

--- python/learner.py
+++ python/learner.py
@@ -11,6 +11,7 @@ import json
 import sys
 import traceback
 import model as M
+import os
 
 class Model(Chain):
     def __init__(self, deepCoder):
@@ -33,6 +34,12 @@ class Dataset(chainer.dataset.DatasetMixin):
     def get_example(self, i):
         return self.data[i]
 
+alpha = float(os.getenv('DEEPCODER_ADAM_ALPHA', 0.001))
+batch_size = int(os.getenv('DEEPCODER_BATCH_SIZE', 100))
+epoch = int(os.getenv('DEEPCODER_EPOCH', 100))
+
+print(alpha, batch_size, epoch)
+
 deepCoder = M.gen_model()
 model = Model(deepCoder)
 
@@ -40,7 +47,7 @@ f = open(sys.argv[1], 'r')
 x = json.load(f)
 y = M.preprocess_json(x)
 
-print(len(y))
+print("Data set size:", len(y))
 
 l1 = [e for e in range(0, len(y)) if e % 100 != 0]
 l2 = [e for e in range(0, len(y)) if e % 100 == 0]
@@ -53,16 +60,16 @@ if len(sys.argv) >= 4:
     gpu = int(sys.argv[3])
 
 try:
-    train_iter = iterators.SerialIterator(train, batch_size=100, shuffle=True)
+    train_iter = iterators.SerialIterator(train, batch_size=batch_size, shuffle=True)
     test_iter = iterators.SerialIterator(test, batch_size=100, repeat=False, shuffle=False)
 
     deepCoder = M.gen_model()
     model = Model(deepCoder)
-    optimizer = optimizers.Adam()
+    optimizer = optimizers.Adam(alpha=alpha)
 
     optimizer.setup(model)
     updater = training.StandardUpdater(train_iter, optimizer, device=gpu)
-    trainer = training.Trainer(updater, (50, 'epoch'), out='result')
+    trainer = training.Trainer(updater, (epoch, 'epoch'), out='result')
 
     trainer.extend(extensions.Evaluator(test_iter, model, device=gpu))
     trainer.extend(extensions.LogReport())
