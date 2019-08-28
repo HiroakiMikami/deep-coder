550ff9e500e785a0d364293a0b265a8c9c9e1f93
Hiroaki Mikami <hiroaki8270@gmail.com>
2019-08-28 14:24:07 +0900

    Use custum converter to use GPUs

--- src/train.py
+++ src/train.py
@@ -1,9 +1,36 @@
 import dataclasses
+import numpy as np
 from typing import List, Union, Dict, Set
 import chainer as ch
 from chainer import training
+from chainer import cuda
 from .model import ModelShapeParameters, Predictor, TrainingClassifier
+from .dataset import ExampleEncoding, PrimitiveEncoding
 
+
+def convert_entry(batch, device):
+    if device is None:
+        def to_device(x):
+            return x
+    elif device < 0:
+        to_device = cuda.to_cpu
+    else:
+        def to_device(x):
+            return cuda.to_gpu(x, device, cuda.Stream.null)
+
+    # convert examples
+    def convert_primitive(primitive: PrimitiveEncoding):
+        return PrimitiveEncoding(to_device(primitive.t), to_device(primitive.value_arr))
+    def convert_example(example: ExampleEncoding):
+        return ExampleEncoding(
+            [convert_primitive(input) for input in example.inputs],
+            convert_primitive(example.output)
+        )
+    def convert_examples(examples: List[ExampleEncoding]):
+        return [convert_example(example) for example in examples]
+
+    return (np.array([convert_examples(examples) for examples, _ in batch]),
+            np.array([to_device(attribute) for _, attribute in batch]))
 
 class Training:
     """
@@ -44,6 +71,6 @@ class Training:
         self.model = TrainingClassifier(self.predictor, w_0)
         opt = optimizer.setup(self.model)
         updater = training.StandardUpdater(
-            train_iter, optimizer, device=device)
+            train_iter, optimizer, device=device, converter=convert_entry)
         self.trainer = training.Trainer(
             updater, (num_epochs, "epoch"), out=out)
